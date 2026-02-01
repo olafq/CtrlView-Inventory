d1abcd5612251edfb5af2ca716984cb75d6eb0d4
Olaf Querol <oquerol@frba.utn.edu.ar>
2026-01-31 21:19:24 -0300

    importacion de porductos ML v2

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -4,7 +4,7 @@ from .catalog_import_run import CatalogImportRun
 from app.db.models.channel import Channel
 from app.db.models.mercadolibre_auth import MercadoLibreAuth
 from .product import Product
-from .sales import Sales
+from .sales import Sale
 from .stock_movement import StockMovement
 __all__ = [
     "Channel",
