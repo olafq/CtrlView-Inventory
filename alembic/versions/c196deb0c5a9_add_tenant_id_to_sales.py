a451303160929737ec53c6229f05708dd0392b2f
Olaf Querol <oquerol@frba.utn.edu.ar>
2026-02-26 19:08:16 -0300

    Tenant v3

--- alembic/versions/c196deb0c5a9_add_tenant_id_to_sales.py
+++ alembic/versions/c196deb0c5a9_add_tenant_id_to_sales.py
@@ -1,36 +1,33 @@
-"""add tenant_id to sales
-
-Revision ID: c196deb0c5a9
-Revises: b60c561de27a
-Create Date: 2026-02-25 23:37:14.290621
-
-"""
-from typing import Sequence, Union
-
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import inspect
 
-
-# revision identifiers, used by Alembic.
-revision: str = 'c196deb0c5a9'
-down_revision: Union[str, Sequence[str], None] = 'b60c561de27a'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision = 'c196deb0c5a9'
+down_revision = 'b60c561de27a'
+branch_labels = None
+depends_on = None
 
 
 def upgrade():
-    op.add_column(
-        "sales",
-        sa.Column("tenant_id", sa.Integer(), nullable=True)
-    )
+    bind = op.get_bind()
+    inspector = inspect(bind)
 
-    op.create_foreign_key(
-        "fk_sales_tenant",
-        "sales",
-        "tenants",
-        ["tenant_id"],
-        ["id"],
-    )
+    columns = [col["name"] for col in inspector.get_columns("sales")]
+
+    if "tenant_id" not in columns:
+        op.add_column(
+            "sales",
+            sa.Column("tenant_id", sa.Integer(), nullable=True)
+        )
+
+        op.create_foreign_key(
+            "fk_sales_tenant",
+            "sales",
+            "tenants",
+            ["tenant_id"],
+            ["id"],
+        )
+
 
 def downgrade():
     op.drop_constraint("fk_sales_tenant", "sales", type_="foreignkey")
